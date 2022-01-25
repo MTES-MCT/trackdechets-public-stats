351b17c92cb5eeae9619c28c45b947945335d889
Colin Maudry <colin@maudry.com>
2022-01-25 10:24:40 +0100

    Weekly stats

--- app.py
+++ app.py
@@ -30,20 +30,37 @@ engine = create_engine(getenv('DATABASE_URL'))
 
 
 @cache.memoize(timeout=cache_timeout)
-def get_bsdd_data() -> pd.DataFrame:
-    df_bsdd_query = pd.read_sql_query(
-        'SELECT id, status, cast("Form"."createdAt" as date), "Form"."isDeleted", cast("Form"."processedAt" as date), '
-        '"Form"."quantityReceived" FROM "default$default"."Form" '
-        'WHERE "Form"."isDeleted" = FALSE AND "Form"."status" <> \'DRAFT\'',
+def get_bsdd_created_data() -> pd.DataFrame:
+    df_bsdd_created_query = pd.read_sql_query(
+        'SELECT date_trunc(\'week\', "default$default"."Form"."createdAt") AS "createdAt", count(*) AS "count" '
+        'FROM "default$default"."Form" '
+        'WHERE "Form"."isDeleted" = FALSE AND "Form"."status" <> \'DRAFT\' '
+        'GROUP BY date_trunc(\'week\', "default$default"."Form"."createdAt") '
+        'ORDER BY date_trunc(\'week\', "default$default"."Form"."createdAt")',
         con=engine)
-    return df_bsdd_query
+    return df_bsdd_created_query
+
+
+@cache.memoize(timeout=cache_timeout)
+def get_bsdd_processed_data() -> pd.DataFrame:
+    df_bsdd_processed_query = pd.read_sql_query(
+        'SELECT date_trunc(\'week\', "default$default"."Form"."processedAt") AS "processedAt", '
+        'sum("default$default"."Form"."quantityReceived") AS "quantityReceived"  '
+        'FROM "default$default"."Form" '
+        'WHERE "Form"."isDeleted" = FALSE AND "Form"."status" <> \'DRAFT\''
+        'GROUP BY date_trunc(\'week\', "default$default"."Form"."processedAt") '
+        'ORDER BY date_trunc(\'week\', "default$default"."Form"."processedAt")',
+        con=engine)
+    return df_bsdd_processed_query
 
 
 @cache.memoize(timeout=cache_timeout)
 def get_company_data() -> pd.DataFrame:
     df_company_query = pd.read_sql_query(
-        'SELECT id, cast("Company"."createdAt" as date)'
-        'FROM "default$default"."Company" ',
+        'SELECT date_trunc(\'week\', "default$default"."Company"."createdAt") AS "createdAt", count(*) as "count" '
+        'FROM "default$default"."Company" '
+        'GROUP BY date_trunc(\'week\', "default$default"."Company"."createdAt") '
+        'ORDER BY date_trunc(\'week\', "default$default"."Company"."createdAt")',
         con=engine)
     return df_company_query
 
@@ -51,44 +68,60 @@ def get_company_data() -> pd.DataFrame:
 @cache.memoize(timeout=cache_timeout)
 def get_user_data() -> pd.DataFrame:
     df_user_query = pd.read_sql_query(
-        'SELECT id, cast("User"."createdAt" as date)'
+        'SELECT date_trunc(\'week\', "default$default"."User"."createdAt") AS "createdAt", count(*) as "count" '
         'FROM "default$default"."User" '
-        'WHERE "User"."isActive" = True',
+        'WHERE "User"."isActive" = True '
+        'GROUP BY date_trunc(\'week\', "default$default"."User"."createdAt") '
+        'ORDER BY date_trunc(\'week\', "default$default"."User"."createdAt")',
         con=engine)
     return df_user_query
 
 
-time_delta = int(getenv('TIME_PERIOD_D'))
-date_n_days_ago = datetime.date(datetime.today() - timedelta(time_delta))
+time_delta_m = int(getenv('TIME_PERIOD_M'))
+time_delta_d = time_delta_m * 30.5
+try:
+    today = datetime.strptime(getenv('FIXED_TODAY_DATE'), '%Y-%m-%d')
+except TypeError:
+    print('Today date is not fixed, using datetime.today()')
+    today = datetime.today()
+date_n_days_ago = today - timedelta(time_delta_d)
 
 # -----------
 # BSDD
 # -----------
 
-df_bsdd: pd.DataFrame = get_bsdd_data()
-df_bsdd_created = df_bsdd[['id', 'createdAt']]
-df_bsdd_created = df_bsdd_created.loc[(datetime.date(datetime.today()) > df_bsdd_created['createdAt'])
+df_bsdd_created: pd.DataFrame = get_bsdd_created_data()
+# TODO integrate these conversions in parse_dates
+df_bsdd_created['createdAt'] = pd.to_datetime(df_bsdd_created['createdAt'], errors='coerce')
+
+df_bsdd_created = df_bsdd_created.loc[(today > df_bsdd_created['createdAt'])
                                       & (df_bsdd_created['createdAt'] >= date_n_days_ago)]
-bsdd_created_daily = px.line(df_bsdd_created.groupby('createdAt').count(), y='id',
-                             title="Nombre de bordereaux de suivi de déchets dangereux (BSDD) créés par jour",
-                             labels={'id': 'Bordereaux de suivi de déchets dangereux',
-                                     'createdAt': 'Date de création'},
-                             markers=True)
-bsdd_created_total = df_bsdd_created.index.size
+print(df_bsdd_created)
+bsdd_created_weekly = px.line(df_bsdd_created, y='count', x='createdAt',
+                              title="Nombre de bordereaux de suivi de déchets dangereux (BSDD) créés par semaine",
+                              labels={'count': 'Bordereaux de suivi de déchets dangereux',
+                                      'createdAt': 'Date de création'},
+                              markers=True)
+bsdd_created_total = df_bsdd_created['count'].sum()
 
 # bsdd_status = px.bar(df_bsdd.groupby(by='status').count().sort_values(['id'], ascending=True), x='id',
 #              title="Répartition des BSDD par statut")
 
 # nb_sent = df_bsdd.query("status=='SENT'")
-df_bsdd_processed = df_bsdd[['id', 'processedAt', 'quantityReceived']]
-df_bsdd_processed = df_bsdd_processed[(datetime.date(datetime.today()) > df_bsdd_processed['processedAt'])
+
+df_bsdd_processed = get_bsdd_processed_data()
+df_bsdd_processed['processedAt'] = pd.to_datetime(df_bsdd_processed['processedAt'], errors='coerce')
+df_bsdd_processed = df_bsdd_processed[(today > df_bsdd_processed['processedAt'])
                                       & (df_bsdd_processed['processedAt'] >= date_n_days_ago)]
-quantity_processed_daily = px.line(df_bsdd_processed.groupby(by='processedAt').sum(),
-                                   title='Quantité de déchets traitée par jour',
-                                   y='quantityReceived',
-                                   labels={'quantityReceived': 'Quantité de déchets traitée (tonnes)',
-                                           'processedAt': 'Date du traitement'},
-                                   markers=True)
+print(df_bsdd_processed)
+
+quantity_processed_weekly = px.line(df_bsdd_processed,
+                                    title='Quantité de déchets traitée par semaine',
+                                    y='quantityReceived',
+                                    x='processedAt',
+                                    labels={'quantityReceived': 'Quantité de déchets traitée (tonnes)',
+                                            'processedAt': 'Date du traitement'},
+                                    markers=True)
 quantity_processed_total = df_bsdd_processed['quantityReceived'].sum().round()
 
 # -----------
@@ -97,20 +130,23 @@ quantity_processed_total = df_bsdd_processed['quantityReceived'].sum().round()
 
 df_company = get_company_data()
 df_company['type'] = 'Établissements'
+df_company['createdAt'] = pd.to_datetime(df_company['createdAt'])
 df_user = get_user_data()
 df_user['type'] = 'Utilisateurs'
-df_company_created = df_company.loc[(datetime.date(datetime.today()) > df_company['createdAt'])
-                                    & (df_company['createdAt'] >= date_n_days_ago)]
-df_user_created = df_user.loc[(datetime.date(datetime.today()) > df_user['createdAt'])
-                              & (df_user['createdAt'] >= date_n_days_ago)]
-df_company_user_created = pd.concat([df_company_created, df_user_created], ignore_index=True)
-company_user_created_daily = px.line(df_company_user_created.groupby(['createdAt', 'type'], as_index=False).count(),
-                                     y='id', x='createdAt', color='type',
-                                     title="Établissements et utilisateurs inscrits par jour",
-                                     labels={'id': 'Inscriptions', 'createdAt': 'Date d\'inscription'},
-                                     markers=True)
-company_created_total = df_company_created.index.size
-user_created_total = df_user_created.index.size
+df_user['createdAt'] = pd.to_datetime(df_user['createdAt'])
+print(df_user)
+print(df_company)
+df_company_user_created = pd.concat([df_company, df_user], ignore_index=True)
+df_company_user_created = df_company_user_created.loc[(today > df_company_user_created['createdAt'])
+                                                      & (df_company_user_created['createdAt'] >= date_n_days_ago)]
+print(df_company_user_created)
+company_user_created_weekly = px.line(df_company_user_created,
+                                      y='count', x='createdAt', color='type',
+                                      title="Établissements et utilisateurs inscrits par semaine",
+                                      labels={'id': 'Inscriptions', 'createdAt': 'Date d\'inscription'},
+                                      markers=True)
+company_created_total = df_company_user_created.loc[df_company_user_created['type'] == 'Établissements']['count'].sum()
+user_created_total = df_company_user_created.loc[df_company_user_created['type'] == 'Utilisateurs']['count'].sum()
 
 
 def add_figure(fig, totals_on_period: [dict], fig_id: str) -> dbc.Row:
@@ -118,7 +154,7 @@ def add_figure(fig, totals_on_period: [dict], fig_id: str) -> dbc.Row:
         result = []
         for dic in totals:
             result += [html.Li(f"{dic['total']} {dic['unit']}")]
-        return [f"Total sur les {time_delta} derniers jours : ", html.Ul(result)]
+        return [f"Total sur les {time_delta_m} derniers mois : ", html.Ul(result)]
 
     row = dbc.Row(
         [
@@ -145,7 +181,7 @@ app.layout = html.Div(children=[
                         "déchet pour tracer les déchets dangereux et/ou polluants (",
                         html.A("POP", href='https://www.ecologie.gouv.fr/polluants-organiques-persistants-pop'),
                         ") produits, ainsi que les différentes étapes de leur traçabilité et ce, jusqu'au traitement "
-                        "final."]),
+                        "final. Les déchets traités à l'étranger ne sont pas tracés par Trackdéchets."]),
                 html.P("Un borderau de suivi de déchet (BSD) est créé pour chaque déchet et il contient de nombreuses "
                        "informations (acteurs, déchets, poids, traitement réalisé etc.). Ces informations sont "
                        "transmises à Trackdéchets par un usage direct ou par API. "),
@@ -155,12 +191,12 @@ app.layout = html.Div(children=[
                        "régulièrement avec de nouvelles statistiques.")
             ]
         ),
-        add_figure(quantity_processed_daily, [{'total': quantity_processed_total, 'unit': "tonnes"}],
-                   "bsdd_processed_daily"),
-        add_figure(bsdd_created_daily, [{'total': bsdd_created_total, 'unit': "bordereaux"}], "bsdd_created_daily"),
-        add_figure(company_user_created_daily, [{'total': company_created_total, 'unit': "établissements"},
-                                                {'total': user_created_total, 'unit': "utilisateurs"}],
-                   "company_user_created_daily"),
+        add_figure(quantity_processed_weekly, [{'total': quantity_processed_total, 'unit': "tonnes"}],
+                   "bsdd_processed_weekly"),
+        add_figure(bsdd_created_weekly, [{'total': bsdd_created_total, 'unit': "bordereaux"}], "bsdd_created_weekly"),
+        add_figure(company_user_created_weekly, [{'total': company_created_total, 'unit': "établissements"},
+                                                 {'total': user_created_total, 'unit': "utilisateurs"}],
+                   "company_user_created_weekly"),
     ]
                   )
 
@@ -170,4 +206,4 @@ if __name__ == '__main__':
     port = getenv('PORT', 8050)
 
     # Scalingo requires 0.0.0.0 as host, instead of the default 127.0.0.1
-    app.run_server(debug=not getenv('PRODUCTION'), host='0.0.0.0', port=int(port))
+    app.run_server(debug=bool(getenv('DEVELOPMENT')), host='0.0.0.0', port=int(port))
