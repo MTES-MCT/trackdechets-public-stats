b574cc5dd0e3629da68d3cf7f0aa430b4f4c8f7f
Colin Maudry <colin@maudry.com>
2022-04-01 15:13:54 +0200

    Reenabled start with 'python run.py'

--- run.py
+++ run.py
@@ -1,3 +1,14 @@
-import app.index
+from app.index import dash_app
+from os import getenv
 
-server = app.index.server
+# To use `gunicorn run:server` (prod)
+server = dash_app.server
+
+# To use `python index.py` (dev)
+if __name__ == "__main__":
+    port = getenv("PORT", "8050")
+
+    # Scalingo requires 0.0.0.0 as host, instead of the default 127.0.0.1
+    dash_app.run_server(debug=bool(getenv("DEVELOPMENT")), host="0.0.0.0", port=int(port))
+
+
